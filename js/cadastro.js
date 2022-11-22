eddee04bf8bcadb637daeda27f5f96249f886e81
Samuel Mesquita da Fonseca <samuelmesquita.df@gmail.com>
2022-11-21 22:08:37 -0300

    feat: validações cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -13,21 +13,43 @@ document.addEventListener("click", e=>{
             email: form.email.value,
             senha: form.senha.value
         };
+
+        const emailValidation = document.getElementById('emailValidation');
+
         const confirmarSenha = form.confirmarSenha.value;
     
-        if(login.email != "" && login.email.length > 10){
-            if(login.senha == confirmarSenha){
-                local.push(login);
-                localStorage.setItem('login',JSON.stringify(local))
-                console.log(localStorage.getItem("login"));
-                window.location.href =  document.getElementById('criarConta').href;
-            } 
-            else{
-                alert("senhas diferente")
-            }
+        if(login.email != "" && login.email.length > 10 && login.senha == confirmarSenha && login.senha != ""){
+            local.push(login);
+            localStorage.setItem('login', JSON.stringify(local))
+            console.log(localStorage.getItem("login"));
+            window.location.href =  document.getElementById('criarConta').href;
         }
         else{
-            alert("Email vazio ou muito pequeno")
+            emailValidation.style.color = 'red';
+            emailValidation.style.fontSize = '.8rem';
+            emailValidation.removeAttribute("hidden");
+            if(login.email == ""){
+                emailValidation.innerHTML = "Email não pode ser vazio."
+            }
+            else if(login.email.length < 10){
+                emailValidation.innerHTML = "Email deve ser maior que 10 caracteres."
+            }
+
+            if(!emailValidation.hasAttribute("hidden") && emailValidation.getAttribute("hidden") != null){
+                emailValidation.setAttribute("hidden");
+            }
+
+            const psValidation = document.getElementById("passwordValidation");
+
+            psValidation.style.color = "red";
+            psValidation.style.fontSize = ".8rem";
+
+            if(login.senha == "" || confirmarSenha == ""){
+                psValidation.innerHTML = 'Senha ou confirmar senha estão vazios.';
+            }
+            else{
+                psValidation.innerHTML = 'Senha esta diferente de confirmar senha.';
+            }
         }
    
        
